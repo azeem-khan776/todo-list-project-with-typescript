7b38dae7aad1e4b64d60acffcf606930ef1d4860
azeem khan <ak98765432email.com@gmail.com>
2024-04-06 16:09:24 +0500

    todo list project

---
+++ todo.ts
@@ -0,0 +1,42 @@
+#! /usr/bin/env node
+import inquirer from "inquirer";
+
+let todostring: string[] = [];
+let loop = true;
+
+interface todoInput{
+    todoItem: string,
+    addmore: boolean
+}
+
+
+while(loop){
+     const todoInput: todoInput = await inquirer.prompt([{
+        type: "input",
+        name: "todoItem",
+        message:"Enter item Name which you want to add in your to-do list"
+     },
+     {
+        type:"confirm",
+        name: "addmore",
+        Message: "Do you want to add more item in your To-Do list",
+     }
+    ]);
+
+    const todoItem = todoInput.todoItem;
+    const addmore = todoInput.addmore;
+if(todoItem){
+    todostring.push(todoItem);
+}
+loop = addmore;
+
+};
+
+if(todostring.length > 0){[]
+    console.log(`more is your To-Do list`);
+    for(let i = 0; i < todostring.length; i++){
+        console.log(`$ (i + 1). ${todostring[i]}`);
+    }
+
+}else{ console.log(`your to-do list is empty!`);
+}
